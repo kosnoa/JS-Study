3eb0c0a97c3664cd4a1c3a88bb2a06aec225df0d
kosnoa <f4less0152@gmail.com>
2022-11-07 00:51:30 -0500

    숙제 완료

--- Level 1/1-9/main.js
+++ Level 1/1-9/main.js
@@ -2,19 +2,18 @@ $('#login').click(function() {
     $('.black-bg').addClass('show-modal');
 });
 
-$(document).ready(function() {
-    $("#close").click(function() {
-        $(".black-bg").removeClass('show-modal');
-    });
+$('#close').click(function() {
+    $('.black-bg').removeClass('show-modal');
 });
 
-$('#send').onclick = function() {
-    if ('#username' === '' || '#pass' === '') {
-        alert('COMPLETE THE BLANK')
+function validation(e) {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "" || password == "") {
+        e.preventDefault();
+        alert("빈칸을 채워주세요")
     } else {
-        $('#move').submit
+        alert("전송되었습니다")
     }
-
-}
-
-// alert('안녕하세요')
+};
